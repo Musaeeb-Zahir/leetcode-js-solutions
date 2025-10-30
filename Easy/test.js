036d391fa41eccf5b6f97c43e23d1a88b9510240
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-10-30 22:17:47 +0500

    solve another problem

---
+++ Easy/test.js
@@ -0,0 +1,16 @@
+let a="1.212.2"
+// let arr=a.split("");
+// console.log(arr);
+// for(let i=0;i<a.length-1;i++){
+//    if(arr[i]=="."){
+    
+//    }
+// }
+ const regex = /[\.]/g;
+ console.log(a.replace(regex,"[.]"));
+
+
+
+
+
+
