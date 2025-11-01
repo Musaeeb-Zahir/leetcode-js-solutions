daf337f45652f84d62f2ab22aad152520b546f74
Musaeeb-zahir <musaeebzahir@gmail.com>
2025-11-01 11:21:09 +0500

    fix some bugs

--- Easy/richestCostumerWealth.js
+++ Easy/richestCostumerWealth.js
@@ -1,18 +1,24 @@
 var maximumWealth = function (accounts) {
-  var m = 0;
+  var arr = [];
+  var sum = 0;
   for (let i = 0; i < accounts.length; i++) {
-    for (let j = 0; j < accounts[i].length; i++) {
-      m += accounts[j];
+    for (let j = 0; j < accounts[i].length; j++) {
+      sum += accounts[i][j];
     }
+    arr[i]=sum;
+    sum=0;
   }
+  
 };
 console.log(
   "the richest person has",
   maximumWealth(
     (accounts = [
-      [1, 5],
-      [7, 3],
-      [3, 5],
+      [
+        [1, 5],
+        [7, 3],
+        [3, 5],
+      ],
     ])
   ),
   "in account"
