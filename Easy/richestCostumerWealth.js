b22b4b2bc770e57c3b894d660a9739d3574cc67a
Musaeeb Zahir <musaeebzahir@example.com>
2025-11-03 22:24:47 +0500

    solve richest custumer problem

--- Easy/richestCostumerWealth.js
+++ Easy/richestCostumerWealth.js
@@ -1,24 +1,24 @@
 var maximumWealth = function (accounts) {
+  console.log(accounts);
+
   var arr = [];
   var sum = 0;
   for (let i = 0; i < accounts.length; i++) {
     for (let j = 0; j < accounts[i].length; j++) {
       sum += accounts[i][j];
     }
-    arr[i]=sum;
-    sum=0;
+    arr[i] = sum;
+    sum = 0;
   }
-  
+  return Math.max(...arr);
 };
 console.log(
   "the richest person has",
   maximumWealth(
     (accounts = [
-      [
-        [1, 5],
-        [7, 3],
-        [3, 5],
-      ],
+      [1, 5],
+      [7, 3],
+      [3, 5],
     ])
   ),
   "in account"
