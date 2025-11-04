0b607145e65d2cdbbec9c7df8f7dbb9b87524fa6
Musaeeb Zahir <musaeebzahir@example.com>
2025-11-05 00:11:47 +0500

    add buildArray function to create distinct array from permutation

---
+++ Easy/BuildArrayFromPermutation.js
@@ -0,0 +1,9 @@
+var buildArray = function (nums) {
+  var ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    ans[i] = nums[nums[i]]; // ans[0]= nums[0]
+  }
+  const distinctArr = new Set(ans);  
+  return [...distinctArr];
+};
+console.log(buildArray([0, 2, 1, 5, 3, 4]));
